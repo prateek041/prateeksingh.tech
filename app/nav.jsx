d55740db23c190926a471fd582b333fb7af5250e
Prateek Singh <prateeksingh9741@gmail.com>
2023-06-12 08:27:27 +0530

    Home page
    Signed-off-by: Prateek Singh <prateeksingh9741@gmail.com>

---
+++ app/nav.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import './page.css';
+
+export default function Home() {
+  return (
+    <div className="home-main">
+      <div className="home-second">
+        <h3 className="home-main-text">Hello I'm</h3>
+        <h1 className="home-second-text">Prateek Singh</h1>
+      </div>
+    </div>
+  );
+}
